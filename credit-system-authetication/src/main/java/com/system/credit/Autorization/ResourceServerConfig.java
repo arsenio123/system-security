14708010636b82c0a3bf0f8d7450d5ad9a293fd8
Arsénio Malagueta <arsenio.malagueta@mozabanco.co.mz>
2024-01-19 16:13:12 +0200

    versios goes with report

--- credit-system-authetication/src/main/java/com/system/credit/Autorization/ResourceServerConfig.java
+++ credit-system-authetication/src/main/java/com/system/credit/Autorization/ResourceServerConfig.java
@@ -1,6 +1,5 @@
 package com.system.credit.Autorization;
 
-//import com.malagueta.backend.credito.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +7,6 @@ import org.springframework.security.access.expression.method.MethodSecurityExpre
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
